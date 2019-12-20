e3f4c9fc516a15b3d0cff6267fba2720567282a5
lvaquero <loreto@notikumi.com>
2019-12-20 18:10:01 +0100

    add convet data

---
+++ js/convertData.js
@@ -0,0 +1,29 @@
+const data = require('../data/containers.json');
+const fs = require('fs');
+
+
+// Construcción del archivo .geoJson para poder servirlo desde nuestro ordenador
+const fromArrayToGeoJSON = (arr) => {
+    const geoJSON = {
+        "type": "FeatureCollection",
+        "features": []
+    };
+    const geoJsonFeatures = geoJSON.features;
+    arr.map((el) => {
+        geoJsonFeatures.push({
+            "type": "Feature",
+            "geometry": {
+                "type": "Point",
+                "coordinates": [el.Lon, el.Lat]
+            },
+            "properties": el
+        })
+    });
+    return geoJSON;
+}
+
+//console.log(JSON.stringify(fromArrayToGeoJSON(data)));
+fs.writeFile('../data/clothing-containers.geojson', JSON.stringify(fromArrayToGeoJSON(data)), (err) => {
+    if (err) console.log(err);
+    console.log("Housting: Successfully Written to File.");
+});
