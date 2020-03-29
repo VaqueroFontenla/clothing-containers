05e21369ae246669124000aa88b78725fa6a34cc
VaqueroFontenla <loret.vaquero@gmail.com>
2020-03-29 19:34:38 +0200

    Fix desktop styles

--- js/main.js
+++ js/main.js
@@ -1,26 +1,29 @@
-var map = L.map("map").setView([42.15, -8.68], 11);
+const ZOOM = 12;
+const MAP_CENTER = [42.2, -8.71];
 
-var layer = L.esri.basemapLayer("Gray").addTo(map);
-var layerLabels;
+const map = L.map("map").setView(MAP_CENTER, ZOOM);
+
+let layer = L.esri.basemapLayer("Gray").addTo(map);
+let layerLabels;
 
 // create the geocoding control and add it to the map
-var searchControl = L.esri.Geocoding.geosearch().addTo(map);
+const searchControl = L.esri.Geocoding.geosearch().addTo(map);
 
 // create geoservice
-var geocodeService = L.esri.Geocoding.geocodeService();
+const geocodeService = L.esri.Geocoding.geocodeService();
 
 // create an empty layer group to store the results and add it to the map
-var results = L.layerGroup().addTo(map);
+const results = L.layerGroup().addTo(map);
 
 // Container Icon
-var baseballIcon = L.icon({
-  iconUrl: "../images/registro.png",
-  iconSize: [6, 6],
+const baseballIcon = L.icon({
+  iconUrl: window.screen.width > 798 ? "../images/trash.png" : "../images/registro.png",
+  iconSize: window.screen.width > 798 ? [24, 24] :[6, 6],
   iconAnchor: [4, 9],
   popupAnchor: [0, -8]
 });
 
-function setBasemap(basemap) {
+const setBasemap = basemap => {
   if (layer) {
     map.removeLayer(layer);
   }
@@ -37,21 +40,20 @@ function setBasemap(basemap) {
     layerLabels = L.esri.basemapLayer(basemap + "Labels");
     map.addLayer(layerLabels);
   }
-}
+};
 
 const onEachFeature = (feature, layer) => {
   if (feature.properties) {
-    var popupContent = ` 
+    const popupContent = ` 
 		<div>
 			<strong> ${feature.properties.Lugar} </strong>
 			  <hr>
 			   <strong>Dirección:  </strong>${feature.properties.Dirección} <br />
 			   <strong>Barrio:  </strong> ${feature.properties.Barrio}
 			  </div>
-			`;
+      `;
+    layer.bindPopup(popupContent);
   }
-
-  layer.bindPopup(popupContent);
 };
 
 const addDataToMap = containers => {
@@ -64,6 +66,19 @@ const addDataToMap = containers => {
   }).addTo(map);
 };
 
+const buildGeocoder = () => {
+  document
+    .querySelector(".geocoder-control")
+    .classList.add("geocoder-control-expanded");
+};
+
+const addHidden = () => {
+  if (window.screen.width < 798) {
+    document.querySelector(".help").classList.add("hidden");
+    document.querySelector(".leaflet-bar").classList.add("hidden");
+    document.querySelector(".geocoder-control").classList.add("hidden");
+  }
+};
 const init = () => {
   fetch("../data/clothing-containers.geojson")
     .then(response => {
@@ -73,19 +88,20 @@ const init = () => {
       const containers = data;
       addDataToMap(containers);
       buildGeocoder();
+      addHidden();
     });
 };
 
 // listen for the results event and add every result to the map
-searchControl.on("results", function(data) {
+searchControl.on("results", data => {
   results.clearLayers();
-  for (var i = data.results.length - 1; i >= 0; i--) {
+  for (let i = data.results.length - 1; i >= 0; i--) {
     results.addLayer(L.marker(data.results[i].latlng));
   }
 });
 
 // listen for to show the closest street address to the clicked point.
-map.on("click", function(e) {
+map.on("click", e => {
   geocodeService
     .reverse()
     .latlng(e.latlng)
@@ -101,40 +117,37 @@ map.on("click", function(e) {
     });
 });
 
-const buildGeocoder = () => {
-  document
-    .querySelector(".geocoder-control")
-    .classList.add("geocoder-control-expanded", "hidden");
-};
-
-document
-  .querySelector(".dashboard__search")
-  .addEventListener("click", function(e) {
-    e.preventDefault();
-    document.querySelector(".geocoder-control").classList.toggle("hidden");
-  });
-
-document
-  .querySelector(".dashboard__basemaps")
-  .addEventListener("click", function(e) {
-    e.preventDefault();
-    document.querySelector("#basemaps-wrapper").classList.toggle("hidden");
-  });
-
-document
-  .querySelector(".dashboard__help")
-  .addEventListener("click", function(e) {
-    e.preventDefault();
-    document.querySelector(".help").classList.toggle("hidden");
-  });
-
-document.querySelector("#basemaps").addEventListener("click", function(e) {
-  e.stopPropagation()
+document.querySelector(".dashboard__search").addEventListener("click", e => {
+  e.preventDefault();
+  document.querySelector(".geocoder-control").classList.toggle("hidden");
 });
 
-document.querySelector("#basemaps").addEventListener("change", function(e) {
-  var basemap = e.target.value;
+document.querySelector(".dashboard__basemaps").addEventListener("click", e => {
+  e.preventDefault();
+  document.querySelector("#basemaps-wrapper").classList.toggle("hidden");
+});
+
+document.querySelector(".dashboard__help").addEventListener("click", e => {
+  e.preventDefault();
+  document.querySelector(".help").classList.toggle("hidden");
+});
+
+document
+  .querySelector("#basemaps")
+  .addEventListener("click", e => e.stopPropagation());
+
+document.querySelector("#basemaps").addEventListener("change", e => {
+  const basemap = e.target.value;
   setBasemap(basemap);
+});
+
+document.querySelector(".help-toggle").addEventListener("click", e => {
+  e.stopPropagation();
+  document.querySelector(".help__content").classList.toggle("hidden");
+});
+
+document.querySelector("#map").addEventListener("click", e => {
+  e.stopPropagation();
 });
 
 init();
