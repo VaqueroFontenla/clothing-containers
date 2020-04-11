a94b1f86608d779172614e8d618e9b31c983939f
VaqueroFontenla <loret.vaquero@gmail.com>
2020-04-11 21:56:42 +0200

    Update dashboard

--- js/main.js
+++ js/main.js
@@ -1,15 +1,22 @@
 const ZOOM = 13;
 const ZOOM_MOBILE = 12;
 const MAP_CENTER = [42.2, -8.71];
-const PLACEHOLDER_GEOCODER = window.screen.width > 798 ? 'Busca o cotenedor que esté máis preto da tua casa' : 'Busca o cotenedor'
-const POSITION_GEOCODER = window.screen.width > 798 ? 'topright' : 'topleft';
+const PLACEHOLDER_GEOCODER =
+  window.screen.width > 798
+    ? "Busca o cotenedor que esté máis preto da tua casa"
+    : "Busca o cotenedor";
+const POSITION_GEOCODER = window.screen.width > 798 ? "topright" : "topleft";
 
-const map = L.map('map').setView(
+const basemaps = document.querySelector("#basemaps");
+const basemapsWrapper = document.querySelector("#basemaps-wrapper");
+const help = document.querySelector(".help");
+
+const map = L.map("map").setView(
   MAP_CENTER,
   window.screen.width > 798 ? ZOOM : ZOOM_MOBILE
 );
 
-let layer = L.esri.basemapLayer('Gray').addTo(map);
+let layer = L.esri.basemapLayer("Gray").addTo(map);
 let layerLabels;
 
 // create the geocoding control and add it to the map
@@ -28,7 +35,7 @@ const results = L.layerGroup().addTo(map);
 // Container Icon
 const baseballIcon = L.icon({
   iconUrl:
-    window.screen.width > 798 ? './images/trash.png' : './images/registro.png',
+    window.screen.width > 798 ? "./images/trash.png" : "./images/registro.png",
   iconSize: window.screen.width > 798 ? [24, 24] : [6, 6],
   iconAnchor: [4, 9],
   popupAnchor: [0, -8],
@@ -47,8 +54,8 @@ const setBasemap = (basemap) => {
     map.removeLayer(layerLabels);
   }
 
-  if (basemap === 'Gray' || basemap === 'DarkGray') {
-    layerLabels = L.esri.basemapLayer(basemap + 'Labels');
+  if (basemap === "Gray" || basemap === "DarkGray") {
+    layerLabels = L.esri.basemapLayer(basemap + "Labels");
     map.addLayer(layerLabels);
   }
 };
@@ -79,16 +86,15 @@ const addDataToMap = (containers) => {
 
 const addHidden = () => {
   if (window.screen.width < 798) {
-    document.querySelector('.help').classList.add('hidden');
-    document.querySelector('.leaflet-control-zoom').classList.add('hidden');
-    document.querySelector('.leaflet-bar').classList.add('hidden');
-    document.querySelector('.geocoder-control-input').classList.add('hidden');
+    document.querySelector(".help").classList.add("hidden");
+    document.querySelector(".leaflet-control-zoom").classList.add("hidden");
+    document.querySelector(".leaflet-bar").classList.add("hidden");
+    document.querySelector(".geocoder-control-input").classList.add("hidden");
   }
 };
 
-
 const init = () => {
-  fetch('./data/clothing-containers.geojson')
+  fetch("./data/clothing-containers.geojson")
     .then((response) => {
       return response.json();
     })
@@ -99,54 +105,74 @@ const init = () => {
     });
 };
 
-// listen for the results event and add every result to the map
-searchControl.on('results', (data) => {
-  results.clearLayers();
-  for (let i = data.results.length - 1; i >= 0; i--) {
-    results.addLayer(L.marker(data.results[i].latlng));
-  }
-});
-
-document.querySelector('.dashboard__search').addEventListener('click', (e) => {
+document.querySelector(".dashboard__search").addEventListener("click", (e) => {
   e.preventDefault();
-  document.querySelector('.geocoder-control-input').classList.toggle('hidden');
+  document.querySelector(".geocoder-control-input").classList.toggle("hidden");
+  if (!basemapsWrapper.classList.contains("hidden")) {
+    basemapsWrapper.classList.add("hidden");
+  }
+  if (!help.classList.contains("hidden")) {
+    help.classList.add("hidden");
+  }
 });
 
 const closeBaseMap = (e) => {
   e.stopPropagation();
-  document.querySelector('#basemaps-wrapper').classList.toggle('hidden');
+  basemapsWrapper.classList.toggle("hidden");
+  if (
+    !document
+      .querySelector(".geocoder-control-input")
+      .classList.contains("hidden")
+  ) {
+    document.querySelector(".geocoder-control-input").classList.add("hidden");
+  }
+  if (!help.classList.contains("hidden")) {
+    help.classList.add("hidden");
+  }
 };
-document
-  .querySelector('.dashboard__basemaps')
-  .addEventListener('click', (e) => closeBaseMap(e));
-document
-  .querySelector('.basemaps__close')
-  .addEventListener('click', (e) => closeBaseMap(e));
 
+document
+  .querySelector(".dashboard__basemaps")
+  .addEventListener("click", (e) => closeBaseMap(e));
+document
+  .querySelector(".basemaps__close")
+  .addEventListener("click", (e) => closeBaseMap(e));
+
+  
 const closeHelp = (e) => {
   e.stopPropagation();
-  document.querySelector('.help').classList.toggle('hidden');
+  help.classList.toggle("hidden");
+  if (
+    !document
+      .querySelector(".geocoder-control-input")
+      .classList.contains("hidden")
+  ) {
+    document.querySelector(".geocoder-control-input").classList.add("hidden");
+  }
+  if (!basemapsWrapper.classList.contains("hidden")) {
+    basemapsWrapper.classList.add("hidden");
+  }
 };
-document
-  .querySelector('.dashboard__help')
-  .addEventListener('click', (e) => closeHelp(e));
-document
-  .querySelector('.help__close')
-  .addEventListener('click', (e) => closeHelp(e));
 
 document
-  .querySelector('#basemaps')
-  .addEventListener('click', (e) => e.stopPropagation());
-document.querySelector('#basemaps').addEventListener('change', (e) => {
+  .querySelector(".dashboard__help")
+  .addEventListener("click", (e) => closeHelp(e));
+document
+  .querySelector(".help__close")
+  .addEventListener("click", (e) => closeHelp(e));
+
+basemaps.addEventListener("click", (e) => e.stopPropagation());
+basemaps.addEventListener("change", (e) => {
   const basemap = e.target.value;
   setBasemap(basemap);
 });
 
-document.querySelector('.help-toggle').addEventListener('click', (e) => {
+document.querySelector(".help-toggle").addEventListener("click", (e) => {
   e.stopPropagation();
-  document.querySelector('.help__content').classList.toggle('hidden');
+  document.querySelector(".help__content").classList.toggle("hidden");
 });
-document.querySelector('#map').addEventListener('click', (e) => {
+
+document.querySelector("#map").addEventListener("click", (e) => {
   e.stopPropagation();
 });
 
